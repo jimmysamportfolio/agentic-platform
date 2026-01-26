cfcaee5f83f836bd8787fe05771ce9ca256e6c87
Jimmy Sam <jimmysam@students.cs.ubc.ca>
2026-01-25 18:58:28 -0800

    add config and requirements

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    BASE_URL = os.getenv("BASE_URL")
+    OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
+    DEFAULT_AI_MODEL = os.getenv("DEFAULT_AI_MODEL")
+
+config = Config()
